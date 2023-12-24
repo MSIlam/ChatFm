3fd8beb922b50dc5ba1c03b3962453ef8d11cc50
MSIlam <saiful.zoology@yahoo.com>
2023-12-24 15:54:17 +0100

    Added chat platform

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,71 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Text, KeyboardAvoidingView } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
   const { name, backgroundColor } = route.params;
+
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#2F4F4F",
+          },
+          left: {
+            backgroundColor: "#FFF8DC",
+          },
+        }}
+      />
+    );
+  };
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "The chat is active now",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
 
   return (
     <View style={[styles.container, { backgroundColor: backgroundColor }]}>
-      <Text>Lets chat</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+      {Platform.OS === "ios" ? (
+        <KeyboardAvoidingView behavior="padding" />
+      ) : null}
     </View>
   );
 };
@@ -18,8 +73,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
